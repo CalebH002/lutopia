b57e824a7ae81e92984600405a8d18cc875a24d3
CalebH002 <cheigl93@gmail.com>
2025-03-30 18:37:31 -0400

    Added help, gallery, and about page

--- color-grading-app/src/App.tsx
+++ color-grading-app/src/App.tsx
@@ -5,12 +5,16 @@ import HomePage from './pages/HomePage';
 import EditorPage from './pages/EditorPage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
+import GalleryPage from './pages/GalleryPage';
+import HelpPage from './pages/HelpPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path= "/help" element={<HelpPage />} />
         <Route path="/editor" element={<EditorPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/404" element={<NotFoundPage />} />
--- color-grading-app/src/Components/Header/Header.tsx
+++ color-grading-app/src/Components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import './Header.css';
 
 const Header: React.FC = () => {
-  const navigate = useNavigate();
+  let navigate = useNavigate(); // Hook to programmatically navigate
 
   return (
     <header className="header">
-      <div className="header-left" onClick={() => navigate('/')}>
-        <img src="/assets/lutopia-icon.png" alt="LUTopia Icon" className="logo-icon" />
-        <img src="/assets/lutopia.png" alt="LUTopia Logo" className="logo-text" />
+      <div className="header-left">
+        <img src="/assets/lutopia-icon.png" alt="LUTopia Icon" className="logo-icon" onClick={() => navigate('/')} />
+        <img src="/assets/lutopia.png" alt="LUTopia Logo" className="logo-text" onClick={() => navigate('/')} />
+
+        <div className="nav-buttons">
+           <Link to="/gallery">Gallery</Link>
+           <Link to="/about">About</Link>
+           <Link to="/help">Help</Link>
+        </div>
+
       </div>
       <div className="header-right">
         {/* Placeholder for user profile icon */}
